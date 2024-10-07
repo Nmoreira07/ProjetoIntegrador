cb0d8db4c8a1150f388baf1bf667b32d15d951d5
Nmoreira07 <nmoreira407@gmail.com>
2024-10-07 20:03:10 -0300

    Adicionado código do sistema nojos

---
+++ nojo´s/src/main/java/br/com/carstore/dao/CarDao.java
@@ -0,0 +1,40 @@
+package br.com.carstore.dao;
+
+import br.com.carstore.model.Car;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+public class CarDao {
+
+    public void createCar(Car car) {
+        String SQL = "INSERT INTO CAR (NAME, COLOR) VALUES (?, ?)";
+
+        try {
+
+            Connection connection = DriverManager.getConnection("jdbc:h2:~/test", "sa","sa");
+
+            System.out.println("success in database connection");
+
+            PreparedStatement preparedStatement = connection.prepareStatement(SQL);
+
+            preparedStatement.setString(1, car.getName());
+            preparedStatement.setString(2, car.getColor());
+            preparedStatement.execute();
+
+            System.out.println("success in insert car");
+
+            connection.close();
+
+        } catch (Exception e) {
+
+            System.out.println("fail in database connection");
+
+        }
+
+
+    }
+
+}
+
